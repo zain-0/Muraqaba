c92418e36a45da8e6c9fc8e91c5d6c0f12314206
Muhammad Zain <zahidzain234@gmail.com>
2025-04-06 20:40:12 +0500

    Fixed Auth Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,6 +45,7 @@ export const registerUser = async (req, res) => {
 
 // Login user
 export const loginUser = async (req, res) => {
+    
     const { email, password } = req.body;
 
     try {
@@ -64,7 +65,7 @@ export const loginUser = async (req, res) => {
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         // Send response with the token
-        res.json({ message: 'Login successful', token });
+        res.json({ token });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error', error });
